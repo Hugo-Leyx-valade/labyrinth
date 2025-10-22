9f994c6e5699663bad28b443c6798aaa41b6032c
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2025-10-22 17:10:05 +0200

    partie 2 fini

---
+++ Labyrinth/Crawl/RandomExplorer.cs
@@ -0,0 +1,71 @@
+﻿using Labyrinth.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth.Crawl
+{
+    internal class RandomExplorer
+    {
+
+        public ICrawler _crawler;
+        public RandomExplorer(ICrawler crawler)
+        {
+            _crawler = crawler;
+        }
+
+        private readonly Random _random = new();
+
+        public bool GetOut(int n)
+        {
+            int i = 0;
+            while (i < n)
+            {
+                // Vérifie si la tuile actuelle est "Outside"
+                if (this._crawler.FacingTile is Outside)
+                {
+                    Console.WriteLine("🚪 Tu es sorti !");
+                    return true;
+                }
+
+                // Choix aléatoire d’une action : 0 = Walk, 1 = TurnRight, 2 = TurnLeft
+                int action = _random.Next(3);
+
+                switch (action)
+                {
+                    case 0:
+                        if (this._crawler.FacingTile.IsTraversable)
+                        {
+                            OnPositionChanged();
+                            this._crawler.Walk();
+                            i++;
+                        }
+                        break;
+                    case 1:
+                        OnDirectionChanged();
+                        this._crawler.Direction.TurnRight();
+                        break;
+                    case 2:
+                        OnDirectionChanged();
+                        this._crawler.Direction.TurnLeft();
+                        break;
+                }
+            }
+
+            Console.WriteLine("❌ Nombre maximal de déplacements atteint sans sortie.");
+            return false;
+        }
+
+        private void OnPositionChanged()
+        {
+            Console.WriteLine($"📍 Position actuelle : ({this._crawler.X}, {this._crawler.Y})");
+        }
+
+        private void OnDirectionChanged()
+        {
+            Console.WriteLine($"🧭 Direction actuelle : {this._crawler.Direction}");
+        }
+    }
+}
--- Labyrinth/Program.cs
+++ Labyrinth/Program.cs
@@ -1,11 +1,33 @@
-﻿Console.WriteLine(new Labyrinth.Labyrinth("""
-    +--+--------+
-    |  /        |
-    |  +--+--+  |
-    |     |k    |
-    +--+  |  +--+
-       |k  x    |
-    +  +-------/|
-    |           |
-    +-----------+
-    """));
+﻿using System;
+using Labyrinth.Crawl;
+
+namespace Labyrinth
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var map = """
+                +--+
+                | x|
+                + -+
+                """;
+
+            try
+            {
+                var labyrinth = new Labyrinth(map);
+                Console.WriteLine(labyrinth.ToString());
+
+                // Crée un crawler et un explorateur aléatoire, puis tente de sortir
+                var crawler = labyrinth.NewCrawler();
+                var explorer = new RandomExplorer(crawler);
+                Console.WriteLine("Début de l'exploration aléatoire...");
+                explorer.GetOut(5); // nombre maximal d'actions
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Erreur lors de l'initialisation : {ex.Message}");
+            }
+        }
+    }
+}
