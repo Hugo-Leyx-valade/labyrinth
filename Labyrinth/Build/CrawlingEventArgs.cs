4d80ff5455e9dc2ca4fbe31e0f0b40d35c9400d4
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2025-10-22 17:10:28 +0200

    partie 3 en cours

---
+++ Labyrinth/Build/CrawlingEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using Labyrinth.Crawl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth.Build
+{
+    public class CrawlingEventArgs: EventArgs
+    {
+
+        public int X { get; }
+        public int Y { get; }
+
+        public Direction Direction { get; set; }
+        public CrawlingEventArgs(int x, int y, Direction direction)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Direction = direction;
+        }
+    }
+}
